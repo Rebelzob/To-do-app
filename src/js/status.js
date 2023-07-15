9f5575c108b8e86617c6b035861ff431e8e64215
Rebelzob <karimdavid07@gmail.com>
2023-07-15 03:46:38 +0100

    Added new file with status function

---
+++ src/js/status.js
@@ -0,0 +1,6 @@
+export const updateCompletionStatus = (taskId, status, tasks) => {
+  tasks[taskId].completed = status;
+  return tasks;
+};
+
+export const clearAllCompleted = (tasks) => tasks.filter(task => !task.completed);
